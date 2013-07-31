33d3beb47cd764ab29286b3b81c77084c347d8b2
vic <vic@10.0.1.37>
2013-07-30 18:53:01 -0700

    Testing git

---
+++ NetBeansProjects/GitTest/src/gittest/GitTest.java
@@ -0,0 +1,13 @@
+
+package gittest;
+
+import javax.swing.JOptionPane;
+
+public class GitTest
+{
+ 
+    public static void main(String[] args)
+    {
+        String showInputDialog = JOptionPane.showInputDialog("Say something.");
+    }
+}
